f5ad05c0f7fa42795dec8fdae75c59129f7b5ec4
Pablo Casale <casalepablo83@gmail.com>
2021-06-13 15:26:53 -0300

    Add first components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CardsComponent } from './components/cards/cards.component';
+import { CreditCardsComponent } from './components/cards/credit-cards/credit-cards.component';
+import { ListCardsComponent } from './components/cards/list-cards/list-cards.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    CreditCardsComponent,
+    ListCardsComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
