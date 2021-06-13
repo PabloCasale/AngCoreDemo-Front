c98ca8fafa4eee97c979caba980dcc9328bcf2c6
Pablo Casale <casalepablo83@gmail.com>
2021-06-13 17:00:54 -0300

    Add form validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -7,6 +8,7 @@ import { CardsComponent } from './components/cards/cards.component';
 import { CreditCardsComponent } from './components/cards/credit-cards/credit-cards.component';
 import { ListCardsComponent } from './components/cards/list-cards/list-cards.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/cards/credit-cards/credit-cards.component.ts
+++ src/app/components/cards/credit-cards/credit-cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {faUserCircle, faCreditCard, faCalendarAlt, faKey, faSave} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -13,10 +14,24 @@ export class CreditCardsComponent implements OnInit {
   faCalendarAlt = faCalendarAlt;
   faKey = faKey;
   faSave = faSave;
+
+  form: FormGroup;
   
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { 
+    this.form = this.formBuilder.group({
+      id: 0,
+      owner: ['', [Validators.required]],
+      cardNumber: ['', [Validators.required, Validators.maxLength(16), Validators.minLength(16)]],
+      expirationDate: ['', [Validators.required, Validators.maxLength(5), Validators.minLength(5)]],
+      cvv: ['', [Validators.required, Validators.maxLength(3), Validators.minLength(3)]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  saveCard() {
+    console.log(this.form);
   }
 
 }
